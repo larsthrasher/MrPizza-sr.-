1ec163442b716612af73e0d4ac3fffefdefeaa33
larsthrasher <thrasherlars@gmail.com>
2020-06-01 15:46:28 -0400

    fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,
+  h2,
+  h3{
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 
 function App() {
   return (
-     <div>Hello Pizza</div>
+    <>
+      <h1>Mr. Pizza</h1>
+      <GlobalStyle/>
+      <div>Hello Pizza</div>
+    </>
   );
 }
 
